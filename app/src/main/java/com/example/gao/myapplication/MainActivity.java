c6cd2fc7abb842de426af3bf85ec4f7f98d85b22
gao <1534216982@qq.com>
2016-12-03 16:54:13 +0800

    Add a button

--- app/src/main/java/com/example/gao/myapplication/MainActivity.java
+++ app/src/main/java/com/example/gao/myapplication/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.gao.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTest = (Button) findViewById(R.id.btn_test);
+        btnTest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Toast.makeText(this,"I am a test button!",Toast.LENGTH_SHORT).show();
     }
 }
